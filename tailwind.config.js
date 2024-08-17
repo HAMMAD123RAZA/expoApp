7968cba20fcd598a90447b3b546c82868eabfa88
Nelson877 <66440163+Nelson877@users.noreply.github.com>
2024-08-17 15:48:13 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
-  content: ["./app./**/*.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
+content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+   theme: {
+    extend: {
+     
+      },
+      fontFamily: {
+      
+      },
+    },
   },
   plugins: [],
 }
